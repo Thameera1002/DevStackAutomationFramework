42e7cdeb5bbdded008859a988e198a4181aa27d8
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-26 19:17:05 +0530

    Update ExtentReportManager.java

--- DevStackAutomationFramework/src/main/java/com/devstack/automation/reporter/ExtentReportManager.java
+++ DevStackAutomationFramework/src/main/java/com/devstack/automation/reporter/ExtentReportManager.java
@@ -2,6 +2,7 @@ package com.devstack.automation.reporter;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
@@ -41,6 +42,18 @@ public class ExtentReportManager {
     public static void logSkip(String message) {
         if(Objects.nonNull(test)) { //test != null
             test.log(Status.SKIP, message);
+        }
+    }
+
+    public static void logFailWithScreenShot(String message,String base64Screenshot) {
+        if(Objects.nonNull(test)) {
+            test.fail(message, MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
+        }
+    }
+
+    public static void flushReport() {
+        if(Objects.nonNull(test)) {
+            extentReports.flush();
         }
     }
 }
