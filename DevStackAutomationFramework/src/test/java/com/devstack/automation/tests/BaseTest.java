ac861f9414a5361a835a15e912cc917fb0c0fc7a
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-26 18:21:11 +0530

    LoginTest & BaseTest created and property file updated

---
+++ DevStackAutomationFramework/src/test/java/com/devstack/automation/tests/BaseTest.java
@@ -0,0 +1,6 @@
+package com.devstack.automation.tests;
+
+import com.devstack.automation.testbase.SeleniumTestBaseHelper;
+
+public class BaseTest extends SeleniumTestBaseHelper {
+}
---
+++ DevStackAutomationFramework/src/test/java/com/devstack/automation/tests/login/LoginTest.java
@@ -0,0 +1,6 @@
+package com.devstack.automation.tests.login;
+
+import com.devstack.automation.tests.BaseTest;
+
+public class LoginTest extends BaseTest {
+}
