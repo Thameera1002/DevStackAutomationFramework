43c29432057d890089612d271bd4f078f070ded9
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-26 18:39:02 +0530

    Update LoginTest.java

--- DevStackAutomationFramework/src/test/java/com/devstack/automation/tests/login/LoginTest.java
+++ DevStackAutomationFramework/src/test/java/com/devstack/automation/tests/login/LoginTest.java
@@ -1,6 +1,15 @@
 package com.devstack.automation.tests.login;
 
+import com.devstack.automation.functions.commons.LIB_Common;
 import com.devstack.automation.tests.BaseTest;
+import com.devstack.automation.utils.PropertyHandler;
+import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
+
+    @Test
+    public void loginTest(){
+        LIB_Common common = new LIB_Common(driver);
+        common.bc_login(PropertyHandler.getProperty("username"), PropertyHandler.getProperty("password"));
+    }
 }
