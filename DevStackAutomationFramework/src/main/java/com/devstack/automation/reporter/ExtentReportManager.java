0b2a661ad4a3a500c818b4b8e631d97b0ff59ca4
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-26 19:11:36 +0530

    Update ExtentReportManager.java

--- DevStackAutomationFramework/src/main/java/com/devstack/automation/reporter/ExtentReportManager.java
+++ DevStackAutomationFramework/src/main/java/com/devstack/automation/reporter/ExtentReportManager.java
@@ -1,11 +1,17 @@
 package com.devstack.automation.reporter;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
+import java.util.Objects;
+
 public class ExtentReportManager {
     private static ExtentReports extentReports;
+
+    private static ExtentTest test;
 
     public static void initReport() {
         extentReports = new ExtentReports();
@@ -14,5 +20,27 @@ public class ExtentReportManager {
         sparkReporter.config().setDocumentTitle("Automation Report");
         sparkReporter.config().setReportName("Test Automation Results");
         extentReports.attachReporter(sparkReporter);
+    }
+
+    public static void createTest(String testName) {
+        test = extentReports.createTest(testName);
+    }
+
+    public static void logPass(String message) {
+        if(Objects.nonNull(test)) { //test != null
+            test.log(Status.PASS, message);
+        }
+    }
+
+    public static void logFail(String message) {
+        if(Objects.nonNull(test)) { //test != null
+            test.log(Status.FAIL, message);
+        }
+    }
+
+    public static void logSkip(String message) {
+        if(Objects.nonNull(test)) { //test != null
+            test.log(Status.SKIP, message);
+        }
     }
 }
