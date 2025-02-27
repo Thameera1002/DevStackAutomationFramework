508f05d40ce648df9dcc75a789fdc1e6d5463437
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-02-27 19:57:15 +0530

    Update TestListener.java

--- DevStackAutomationFramework/src/main/java/com/devstack/automation/testbase/TestListener.java
+++ DevStackAutomationFramework/src/main/java/com/devstack/automation/testbase/TestListener.java
@@ -1,6 +1,10 @@
 package com.devstack.automation.testbase;
 
 import com.devstack.automation.reporter.ExtentReportManager;
+import com.devstack.automation.utils.ThreadLocalWebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -15,6 +19,10 @@ public class TestListener implements ITestListener {
     }
 
     public void onTestFailure(ITestResult result) {
+        WebDriver driver = ThreadLocalWebDriverManager.getDriver();
+        String base64Screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+        ExtentReportManager.logFailWithScreenShot("Test Failed."+result.getThrowable().getLocalizedMessage(), base64Screenshot);
+        ExtentReportManager.logFail(result.getMethod().getMethodName()+" Failed.");
     }
 
     public void onTestSkipped(ITestResult result) {
